9a8e79860bc1c85c23674895ce3117f77cc19b21
LincT <joseph.dwr.martin@gmail.com>
2017-03-23 10:38:52 -0500

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ch2Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExponent = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(104, 23);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(81, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Window;
+            this.btnAdd.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(17, 83);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(55, 52);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "&+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.BackColor = System.Drawing.SystemColors.Window;
+            this.btnSubtract.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(67, 83);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(55, 52);
+            this.btnSubtract.TabIndex = 2;
+            this.btnSubtract.TabStop = false;
+            this.btnSubtract.Text = "&-";
+            this.btnSubtract.UseVisualStyleBackColor = false;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.BackColor = System.Drawing.SystemColors.Window;
+            this.btnMultiply.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(17, 141);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(55, 52);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.TabStop = false;
+            this.btnMultiply.Text = "&*";
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.SystemColors.Window;
+            this.btnDivide.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(67, 141);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(55, 52);
+            this.btnDivide.TabIndex = 4;
+            this.btnDivide.TabStop = false;
+            this.btnDivide.Text = "&/";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(68, 197);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 52);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.BackColor = System.Drawing.SystemColors.Window;
+            this.txtInput2.Location = new System.Drawing.Point(104, 42);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(81, 20);
+            this.txtInput2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(14, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "First Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(14, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Second Value";
+            // 
+            // btnExponent
+            // 
+            this.btnExponent.BackColor = System.Drawing.SystemColors.Window;
+            this.btnExponent.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExponent.Location = new System.Drawing.Point(118, 141);
+            this.btnExponent.Name = "btnExponent";
+            this.btnExponent.Size = new System.Drawing.Size(55, 52);
+            this.btnExponent.TabIndex = 10;
+            this.btnExponent.TabStop = false;
+            this.btnExponent.Text = "&^";
+            this.btnExponent.UseVisualStyleBackColor = false;
+            this.btnExponent.Click += new System.EventHandler(this.btnExponent_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(118, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 52);
+            this.button1.TabIndex = 11;
+            this.button1.TabStop = false;
+            this.button1.Text = "/&r";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClr
+            // 
+            this.btnClr.BackColor = System.Drawing.SystemColors.Window;
+            this.btnClr.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClr.Location = new System.Drawing.Point(17, 197);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(55, 52);
+            this.btnClr.TabIndex = 12;
+            this.btnClr.TabStop = false;
+            this.btnClr.Text = "&AC";
+            this.btnClr.UseVisualStyleBackColor = false;
+            this.btnClr.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnExponent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basic calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExponent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClr;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch2Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {//removed button
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {//addition
+            decimal input1;
+            decimal input2;
+            if (!(Decimal.TryParse(txtInput.Text, out input1)))
+            {
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                txtInput.Focus();
+            }
+            else if (!(Decimal.TryParse(txtInput2.Text, out input2)))
+            {
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = Convert.ToString(input1 + input2);
+                MessageBox.Show(result, "Result");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {//subtraction
+            decimal input1;
+            decimal input2;
+            if (!(Decimal.TryParse(txtInput.Text, out input1)))
+            {
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                txtInput.Focus();
+            }
+            else if (!(Decimal.TryParse(txtInput2.Text, out input2)))
+            {
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = Convert.ToString(input1 - input2);
+                MessageBox.Show(result, "Result");
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {//multiplication
+            decimal input1;//variable declaration
+            decimal input2;
+
+            //data validation
+            if (!(Decimal.TryParse(txtInput.Text, out input1)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput.Focus();
+            }
+            else if (!(Decimal.TryParse(txtInput2.Text, out input2)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = Convert.ToString(input1 * input2);
+                MessageBox.Show(result, "Result");
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {//division
+            decimal input1;
+            decimal input2;
+            if (!(Decimal.TryParse(txtInput.Text, out input1)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput.Focus();
+            }
+            else if (!(Decimal.TryParse(txtInput2.Text, out input2)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = Convert.ToString(input1 / input2);
+                MessageBox.Show(result, "Result");
+            }
+
+        }
+
+       
+
+        private void btnExponent_Click(object sender, EventArgs e)
+        {
+            //exponentials
+            double input1;
+            double input2;
+            if (!(Double.TryParse(txtInput.Text, out input1)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput.Focus();
+            }
+            else if (!(Double.TryParse(txtInput2.Text, out input2)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = Convert.ToString(Math.Pow(input1,input2));
+                MessageBox.Show(result, "Result");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                //modulus calculation
+            float input1;
+            float input2;
+            if (!(float.TryParse(txtInput.Text, out input1)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput.Focus();
+            }
+            else if (!(float.TryParse(txtInput2.Text, out input2)))
+            {
+                //display error on bad data
+                MessageBox.Show("Please Enter Numeric Value", "Error!");
+                //return to input box with bad data to attempt new submission
+                txtInput2.Focus();
+            }
+            else
+            {
+                string result = (input1/input2).ToString("n0") + " r: " + 
+                    (input1 - (input1 % input2)).ToString("n0");
+                MessageBox.Show(result, "Result");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtInput2.Text = "";
+        }
+    }
+}
